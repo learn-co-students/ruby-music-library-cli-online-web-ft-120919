ad0e3e4c86c386b3e6bb595df6516ddd59bb1c98
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-06 15:42:46 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
     
      if genre = Genre.find_by_name(input)
       genre.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
-        puts "#{i}. #{song.name} - #{song.genre.name}"
+        puts "#{i}. #{song.name} - #{song.artist.name}"
       end
     end
   end
