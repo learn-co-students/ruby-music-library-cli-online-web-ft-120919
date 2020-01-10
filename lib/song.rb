d3b8b1f5a988a32112564eb971eb51320dd1a0aa
asengelhart <asengelhart@yahoo.com>
2020-01-10 12:46:01 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist unless artist.nil?
+    self.genre = genre unless genre.nil?
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+end
